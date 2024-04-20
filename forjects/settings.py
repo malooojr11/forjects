c623ea0b6af736865c39f45d3c7b8366f5352b6e
malooojr11 <mohtarek620@gmail.com>
2024-04-20 02:42:21 +0200

    render setup

--- forjects/settings.py
+++ forjects/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,15 +82,17 @@ WSGI_APPLICATION = 'forjects.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'default': dj_database_url.config(
+        #Replace this value with your local database's connection string.
+         default='postgresql://postgres:postgres@localhost:5432/mysite',
+         conn_max_age=600),
         'NAME': 'forjects',
         'USER': 'postgres',
         'PASSWORD': '100200300',
         'HOST': 'localhost',
         'PORT': ''
     }
-}
+
 
 
 # Password validation
@@ -129,7 +132,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -144,9 +153,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-
-INTERNAL_IPS = [
-
-    "127.0.0.1"
-
-]
