e7a774b3c188aa0a087a9d4ab50a7a430bb57ab9
malooojr11 <mohtarek620@gmail.com>
2024-04-23 07:22:00 +0200

    render setup

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python forjects/manage.py createsuperuser --no-input
-fi
