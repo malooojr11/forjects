8ece3b610ca74fdd614ad9f9554c25594184d5bd
malooojr11 <mohtarek620@gmail.com>
2024-04-29 18:36:09 +0300

    Render Settings

--- forjects/settings.py
+++ forjects/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-=so7@t$@=u(5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
